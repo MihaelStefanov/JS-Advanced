0b54e9b7eccf576d5c3dd8d02419f95d88eb36ad
Mihael <145503588+MihaelStefanov@users.noreply.github.com>
2025-10-24 00:19:24 +0300

    Add files via upload

---
+++ 02 Arrays and Nested Arrays Lab/03. Sum First Last .js
@@ -0,0 +1,10 @@
+function solve(inputArray) {
+    let firstEl = Number(inputArray[0]);
+    let lastEl = Number(inputArray[inputArray.length -1]);
+
+    let sum = firstEl + lastEl;
+
+    console.log(sum);
+}
+
+solve(['20', '30', '40'] );
